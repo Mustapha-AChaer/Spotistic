10bec0e0f6589aa8b73d38626ae87c6807b3ca50
Mustapha-AChaer <Mustapha.abouchaar@lau.edu>
2021-12-12 21:24:06 +0200

    added album list

--- src/app/album-list/album-list.page.ts
+++ src/app/album-list/album-list.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlbumListService } from './album-list.service';
+import { Album } from './album-list.service';
 
 @Component({
   selector: 'app-album-list',
@@ -12,16 +13,27 @@ export class AlbumListPage implements OnInit {
 
   private name: string;
   private imgUrl: string;
+  private albums: Album[];
   constructor(private route:ActivatedRoute, private router: Router, private service: AlbumListService) { 
     this.route.queryParams.subscribe(params =>{
       if(params){
         this.name = params.artistName;
         this.imgUrl = params.imageUrl;
+        this.getAlbums(this.name);
         console.log(this.name +  this.imgUrl);
       }
     })
   }
 
-  ngOnInit() {  }
+  ngOnInit() {  
+  }
+
+  getAlbums(name: string){
+    return this.service.getAllAlbums(name).subscribe(response=>{
+      this.albums = response;
+    })
+  }
+
+  
 
 }
--- src/app/album-list/album-list.service.ts
+++ src/app/album-list/album-list.service.ts
@@ -1,15 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface Album{
+  albumName: string
+}
 
+export interface ArtistName{
+  artistName: string
+}
 @Injectable({
   providedIn: 'root'
 })
 export class AlbumListService {
 
+  private artist: ArtistName;
   private base_url ='http://localhost/FinalMobileProject/getAllAlbums.php'
 
   constructor(private http : HttpClient) { }
 
+  getAllAlbums(naming: string){
+    this.artist = {
+      artistName: naming
+    }
+    return this.http.post<[Album]>(this.base_url, this.artist);
+  }
  
 }
